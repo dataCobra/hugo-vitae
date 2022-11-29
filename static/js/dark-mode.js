9bdf1789eefe2a2be27be8614263d0be5f9f6b3d
Konrad Borowski <konrad@borowski.pw>
2022-11-29 16:57:52 +0100

    Use a button for switching dark mode
    Buttons unlike links without href attribute can be sequentially
    navigated using keyboard. Considering dark mode switch doesn't lead
    anywhere, using a button is more semantically appropriate.

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -1,12 +1,12 @@
-const toggleSwitch = document.querySelector('i.dark-mode.icons.fas');
+const toggleSwitch = document.querySelector('#dark-mode');
 var currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
     if (currentTheme === 'dark') {
-        document.querySelector('i.dark-mode.icons.fas').classList.remove('fa-moon');
-        document.querySelector('i.dark-mode.icons.fas').classList.add('fa-sun');
+        document.querySelector('#dark-mode .icons.fas').classList.remove('fa-moon');
+        document.querySelector('#dark-mode .icons.fas').classList.add('fa-sun');
     }
 } else {
     currentTheme = localStorage.setItem('theme', 'light')
@@ -18,14 +18,14 @@ function switchTheme() {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
         currentTheme = localStorage.getItem('theme');
-        document.querySelector('i.dark-mode.icons.fas').classList.remove('fa-moon');
-        document.querySelector('i.dark-mode.icons.fas').classList.add('fa-sun');
+        document.querySelector('#dark-mode .icons.fas').classList.remove('fa-moon');
+        document.querySelector('#dark-mode .icons.fas').classList.add('fa-sun');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
         currentTheme = localStorage.getItem('theme');
-        document.querySelector('i.dark-mode.icons.fas').classList.remove('fa-sun');
-        document.querySelector('i.dark-mode.icons.fas').classList.add('fa-moon');
+        document.querySelector('#dark-mode .icons.fas').classList.remove('fa-sun');
+        document.querySelector('#dark-mode .icons.fas').classList.add('fa-moon');
     }
 }
 
